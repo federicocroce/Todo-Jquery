337d9438b237999be77227fe10dc989d51575a03
federicocroce <fede.croce.123@gmail.com>
2014-01-10 11:00:09 -0300

    no message

--- Script.js
+++ Script.js
@@ -1,44 +1,36 @@
 ﻿
 $(document).ready(function () {
 
-	$('#time').jTime();
 
-	var totalAmount;
-	var completeAmount;
-	var toDoAmount;
 
-	//Agregar elemento
+
+	var elemntsList;
+
 	$('#add').click(function () {
-		//Almacenos el valor del Txt en la variable Description.
+
 		var Description = $('#description').val();
-		//Si Description esta vacia.
+
 		if ($("#description").val() == '') {
-			//Anima un label y retorna falso
 			$('#alert').html("<strong>Warning!</strong> You left the to-do empty");
 			$('#alert').fadeIn().delay(1000).fadeOut();
 			return false;
 		}
-		//Agrega elemento a la lista TODOS con la descripción del txt.
+
 		$('#todos').prepend("<li class='item'><input id='check' class='check' name='check' type='checkbox'/>" + Description + "</li>");
 
-		totalAmount += 1;
-		toDoAmount = totalAmount - completeAmount;
-//		console.debug('Antes:' + (totalAmount - completeAmount));
-//		console.debug('Total: ' + totalAmount + '   Completadas: ' + completeAmount + '   Pendientes: ' + toDoAmount);
-		$('#lblTotal').html(totalAmount);
-		$('#lblToDo').html(toDoAmount);
+		mInit.increasesTotalAmount();
+
+		mInit.updateVarPublic();
 		$('#form')[0].reset();
-//		console.debug('Despues:' + (totalAmount - completeAmount));
-//		console.debug('Total: ' + totalAmount + '   Completadas: ' + completeAmount + '   Pendientes: ' + toDoAmount);
-		//Obtengo el HTML de todos los TODOS y lo almaceno en una variable
+
 		var todos = $('#todos').html();
-		//Almaceno en el local Storage con la Key todos todos los elementos.
+
 		localStorage.setItem('todos', todos);
 		return false;
 	});
 
 	if (localStorage.getItem('todos')) {
-//		console.log('Entra el getitem')
+		//		console.log('Entra el getitem')
 		$('#todos').html(localStorage.getItem('todos'));
 	}
 
@@ -48,9 +40,26 @@ $(document).ready(function () {
 		return false;
 	});
 
+
+
+	$('#deleteSelec').click(function () {
+
+		$('#todos li').each(function () {
+			if ($(this).hasClass("lineThrough")) {
+				$(this).remove();
+			}
+		});
+
+
+		var todos = $('#todos').html();
+		localStorage.setItem('todos', todos);
+		location.reload();
+		mInit.updateVarPublic();
+	});
+
+
 	$(document).on('click', '.check', function (e) {
-		
-//		console.log('Antes:' + toDoAmount)
+
 		if ($(this).parent().hasClass("lineThrough")) {
 			$(this).parent().removeClass("lineThrough");
 
@@ -59,56 +68,76 @@ $(document).ready(function () {
 
 		} else {
 			$(this).parent().addClass("lineThrough");
-			completeAmount += 1;
-			toDoAmount -= 1;
+			mInit.increasesCompleteAmount();
 
 		}
-//		console.log('Despues:' + toDoAmount)
+		//		console.log('Despues:' + toDoAmount)
+		mInit.updateVarPublic();
+	});
+
+
+
+	elemntsList = $('#todos li').size();
+
+	mInit.initVarPublic(elemntsList);
+
+	mInit.updateVarPublic();
+});
+
+
+
+
+var mInit = (function () {
+
+	var totalAmount = 0;
+	var completeAmount = 0;
+	var toDoAmount = 0;
+
+	var initVarPrivate = function (elemntsList) {
+		totalAmount = elemntsList;
+		completeAmount = 0;
+		toDoAmount = totalAmount;
+	};
+
+	var updateVarPrivate = function () {
+		$('#lblTotal').html(totalAmount);
 		$('#lblComplete').html(completeAmount);
 		$('#lblToDo').html(toDoAmount);
+	};
 
+	var increasesTotalAmountP = function () {
+		totalAmount += 1;
+		toDoAmount = totalAmount - completeAmount;
+	};
 
-		//	  
-		//	  if ($(this).is(':checked')) {
-		//            $(this).parent().addClass("lineThrough");
-		//            console.log('Antes:'+ toDoAmount)
-		//            toDoAmount -= 1;
-		//            completeAmount += 1;
-		//			console.log('Despues:'+ toDoAmount)
-		//        } else {
-		//            $(this).parent().removeClass('lineThrough');
-		//
-		//            completeAmount -= 1;
-		//            toDoAmount += 1;
-		//        }
-		//        $('#lblComplete').html(completeAmount);
-		//        $('#lblToDo').html(toDoAmount);
-	});
+	var increasesCompleteAmountP = function () {
+		completeAmount += 1;
+		toDoAmount -= 1;
+	};
 
+	var increasesToDoAmountP = function () {
+		completeAmount -= 1;
+		toDoAmount += 1;
 
-	$('#deleteSelec').click(function () {
-		$('#todos li').each(function () {
-			if ($(this).hasClass("lineThrough")) {
-				$(this).remove();
-			}
-		});
-		
-//		toDoAmount = 0;
-//		$('#lblToDo').html(toDoAmount);
-		var todos = $('#todos').html();
-		localStorage.setItem('todos', todos);
-		location.reload();
-	});
+	};
 
+	return {
 
+		initVarPublic: function (elemntsList) {
+			return initVarPrivate(elemntsList);
+		},
+		updateVarPublic: function () {
+			return updateVarPrivate();
+		},
+		increasesTotalAmount: function () {
+			return increasesTotalAmountP();
+		},
+		increasesCompleteAmount: function () {
+			return increasesCompleteAmountP();
+		},
+		increasesToDoAmount: function () {
+			return increasesToDoAmountP();
+		},
 
-//	console.log('Q pasa aca?')
-
-	totalAmount = $('#todos li').size();
-	completeAmount = 0;
-	toDoAmount = totalAmount;
-
-	$('#lblTotal').html(totalAmount);
-	$('#lblComplete').html(completeAmount);
-	$('#lblToDo').html(toDoAmount);
-});
+	}
+})();
